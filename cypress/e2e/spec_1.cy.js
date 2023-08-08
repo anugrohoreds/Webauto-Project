f5789c08219b1597671f4425c73956fcf5232391
anugrohoreds <agung251295@gmail.com>
2023-08-08 13:48:44 +0700

    second commit

--- cypress/e2e/spec_1.cy.js
+++ cypress/e2e/spec_1.cy.js
@@ -10,4 +10,26 @@ describe("Magento Project",()=>{
         cy.get("#password-confirmation").type("@Jeffscott12345")
         cy.get("#form-validate > .actions-toolbar > div.primary > .action > span").click()
     })
+
+    it("Sign-Up",()=>{
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Create an Account").click()
+        cy.get("#firstname").type("Jeff")
+        cy.get("#lastname").type()
+        cy.get("#email_address").type("jeffscott@gmail.com")
+        cy.get("#password").type("@Jeffscot12345")
+        cy.get("#password-confirmation").type("@Jeffscott12345")
+        cy.get("#form-validate > .actions-toolbar > div.primary > .action > span").click()
+    })
+
+     it("Sign-Up",()=>{
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Create an Account").click()
+        cy.get("#firstname").type()
+        cy.get("#lastname").type("Scott")
+        cy.get("#email_address").type("jeffscott@gmail.com")
+        cy.get("#password").type("@Jeffscot12345")
+        cy.get("#password-confirmation").type("@Jeffscott12345")
+        cy.get("#form-validate > .actions-toolbar > div.primary > .action > span").click()
+    })
 })
